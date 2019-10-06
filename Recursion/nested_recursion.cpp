be6ba7c2ebe4ae2918b0bca6f7bf68f24354f665
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-10-06 13:28:42 +0530

    recursion programs added

---
+++ Recursion/nested_recursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int fun(int n)
+{
+    if(n>100)
+    {
+        return n-10;
+    }
+    else
+    {
+        return fun(fun(n+11));
+    }
+}
+
+int main()
+{
+    int r=30;
+    cout<<fun(r);
+    return 0;
+}
