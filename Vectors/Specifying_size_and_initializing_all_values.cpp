257d91db1527dab9c08ab4dd4d5804c824439c2d
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-10-12 12:03:02 +0530

    Add files via upload

---
+++ Vectors/Specifying_size_and_initializing_all_values.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    vector<int> v(n, 10);
+    for(int x : v)
+        cout<<x<<" ";
+    return 0;
+}
+
+
+// Output -> 10 20 30 40 50
