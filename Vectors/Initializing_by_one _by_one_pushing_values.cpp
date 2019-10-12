5d309e94b9f724c9935c96e23816954f52662903
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-10-12 11:56:24 +0530

    vectors added in c++

---
+++ Vectors/Initializing_by_one _by_one_pushing_values.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
+    for(int x : v)
+    {
+        cout<<x<<" ";
+    }
+    return 0;
+}
+
+
+// Output -> 10 20 30 40 50
