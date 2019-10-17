676f0ba26e10e72a93971df8098e980284a1a300
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-10-18 00:19:48 +0530

    Array section added

---
+++ Arrays/array_rotate.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void leftRotatedbyOne(int arr[], int n)
+{
+    int temp = arr[0],i;
+    for(i=0;i<n-1;i++)
+    {
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+}
+
+void leftRotate(int arr[], int d, int n)
+{
+    for(int i=0;i<d;i++)
+        leftRotatedbyOne(arr,n);
+}
+
+void printArray(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    //function calling
+    leftRotate(arr,2,n);
+    printArray(arr,n);
+    return 0;
+}
