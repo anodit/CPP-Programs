ebec703fc11d9d9c659ab0d3eabcbd2f46579c7a
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2020-09-25 17:34:13 +0530

    initial

---
+++ Miscellaneous/HighestPowerof2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,ans;
+    cout<<"Enter the No. n : ";
+    cin>>n;
+    for(int i=n;i>=1;i--)
+    {
+        if((i&(i-1))==0)
+        {
+            ans=i;
+            break;
+        }
+    }
+    cout<<"Ans :"<<1ans<<endl;
+    return 0;
+
+}
